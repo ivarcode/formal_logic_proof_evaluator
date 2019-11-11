1a7c9af947f825c78438679b505b7d6d8f090cfe
Nicolas Holmgren <nicolasholmgren@Nicolass-MacBook-Pro.local>
2019-11-10 20:20:52 -0500

    created MT and a tester for it

--- logic_proof_algorithm/src/net/ivarcode/camden/TestCases.java
+++ logic_proof_algorithm/src/net/ivarcode/camden/TestCases.java
@@ -29,6 +29,36 @@ public class TestCases {
 
 		System.out.println(proof);
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		Proof tollensProof = new Proof();
+		tollensProof.loadPremises("~B,A->B");
+		tollensProof.setConsequent(new Line("~A", 0));
+		
+		//System.out.println(proof.getLine(0));
+		//System.out.println(proof.getLine(1));
+		
+//		Proof pe = new Proof("A,A->B", "B");
+		
+		//System.out.println(proof);
+		
+		// prove recursively until exhausted options
+		
+//		proof.prove();
+		int b = 1;//start
+		while (b != 0) {
+			b = tollensProof.prove();
+			System.out.println("full pass :: " + b);
+		}
+
+		System.out.println(tollensProof);
+		
 	}
 
 }
