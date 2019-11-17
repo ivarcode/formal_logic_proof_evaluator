22450fd832d6efe2da9d103605ed592c1dd23e36
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-11-17 16:31:41 -0500

    setting up test case

--- logic_proof_algorithm/src/net/ivarcode/camden/TestCases.java
+++ logic_proof_algorithm/src/net/ivarcode/camden/TestCases.java
@@ -12,7 +12,7 @@ public class TestCases {
 //		proof.loadPremises("~(A&B),~F,(A&B)vF");
 //		proof.setConsequent(new Line("F", 0));
 		
-		Proof proof = new Proof("C,C->X","C");
+		Proof proof = new Proof("~X,C->X","C");
 		System.out.println(proof.getLine(0));
 //		Proof proof = new Proof("A,A->B,A->(CvD),(CvD)->Z","B");
 		
